16898278192409cbac30c19c2735d2d2665061a5
Kymaito <66735944+Kymaito@users.noreply.github.com>
2023-04-18 16:42:50 +0200

    Create fine.lua

---
+++ fine.lua
@@ -0,0 +1,6 @@
+Realistic_Police.FiningPolice[1] = { 
+    ["Name"] = "", -- le nom de l'amende
+    ["Price"] = 1000, -- le prix a payer
+    ["Vehicle"] = false, -- peut être mis sur une voiture
+    ["Category"] = "General", -- la categorie
+}
